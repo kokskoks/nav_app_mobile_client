d38035ead7fad7a9b600f39c8058d09a78e23b96
Łukasz Świtoń <lukas06.04.93@gmail.com>
2016-10-16 16:40:57 +0200

    Added OSM support

--- app/src/main/java/pl/lodz/p/navapp/MainActivity.java
+++ app/src/main/java/pl/lodz/p/navapp/MainActivity.java
@@ -3,11 +3,27 @@ package pl.lodz.p.navapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+
 public class MainActivity extends AppCompatActivity {
+    private MapView mMapView;
+    private MapController mMapController;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.mapview);
+        mMapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+        mMapView.setBuiltInZoomControls(true);
+        mMapView.setMaxZoomLevel(19);
+        mMapController = (MapController) mMapView.getController();
+        mMapController.setZoom(14);
+        GeoPoint gPt = new GeoPoint(51.746340, 19.450110);
+        mMapController.setCenter(gPt);
     }
+
 }
