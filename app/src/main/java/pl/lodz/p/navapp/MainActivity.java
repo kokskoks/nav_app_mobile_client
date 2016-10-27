afeb21377259ceb1b4a0c7992a2e477ebfbdbba1
Łukasz Świtoń <lukas06.04.93@gmail.com>
2016-10-27 21:28:10 +0200

    Git add added marker functionality

--- app/src/main/java/pl/lodz/p/navapp/MainActivity.java
+++ app/src/main/java/pl/lodz/p/navapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
@@ -14,6 +16,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
@@ -37,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
     LocationManager locationManager;
     final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
     List<PlaceInfo> placesList;
+    AutoCompleteTextView autocompleteLocation;
+    String[] names;
+    String[] addresses;
+    double[] lat;
+    double[] lon;
+    List<Drawable> images;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,8 +59,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setupMap();
         createData();
-        addMarkers();
 
+        autocompleteLocation = (AutoCompleteTextView) findViewById(R.id.autocompleteLocation);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+        autocompleteLocation.setAdapter(adapter);
+        autocompleteLocation.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                addMarker(i);
+            }
+        });
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
         FloatingActionButton fabLocation = (FloatingActionButton) findViewById(R.id.fabLocation);
@@ -80,21 +99,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createData() {
-        placesList.add(new PlaceInfo("WydziaĹ‚ Elektrotechniki Elektroniki Informatyki i Automatyki","Stefanowskiego 18/22",51.75252299,19.45303313));
-        placesList.add(new PlaceInfo("WydziaĹ‚ Mechaniczny","Stefanowskiego 1/15",51.75284959,19.45258552));
-        placesList.add(new PlaceInfo("WydziaĹ‚ Chemiczny","Ĺ»eromskiego 116",51.75365549,19.45091179));
-        placesList.add(new PlaceInfo("WydziaĹ‚ Mechaniczny","Stefanowskiego 1/15",51.75373305,19.45180004));
-        placesList.add(new PlaceInfo("WydziaĹ‚ Mechaniczny - Fabryka InĹĽynierĂłw","Stefanowskiego 2",51.75495143, 19.45124336));
-        placesList.add(new PlaceInfo("Katedra WĹ‚Ăłkien Sztucznych","Ĺ»eromskiego 116",51.75437321,19.45109669));
-        placesList.add(new PlaceInfo("Instytut Chemi OgĂłlnej i Ekologicznej","Ĺ»eromskiego",51.75424923,19.45077855));
-        placesList.add(new PlaceInfo("Centrum KsztaĹ‚cenia MiÄ™dzynarodowego","Ĺ»wirki 36",51.75503501,19.45152691));
-        placesList.add(new PlaceInfo("WydziaĹ‚ Biotechnologii i nauk o ĹĽywnoĹ›ci","Stefanowskiego",51.75472001,19.45264238));
-        placesList.add(new PlaceInfo("Katedra aparatury przemysĹ‚owej","Stefanowskiego 12/16",51.75370351,19.4529498));
-        placesList.add(new PlaceInfo("Instytut Mechatroniki i SystemĂłw Informatycznych","Stefanowskiego 18/22",51.7537103,19.45394666));
-        placesList.add(new PlaceInfo("WydziaĹ‚ InĹĽynierii Procesowej i Ochorny Ĺšrodowiska","WĂłlczaĹ„ska 213",51.75412276,19.45414356));
-        placesList.add(new PlaceInfo("WydziaĹ‚ Biotechnologii i nauk o ĹĽywnoĹ›ci","WĂłlczaĹ„ska 171/173",51.75448702,19.45450147));
-        placesList.add(new PlaceInfo("Rekrutacja","Stefanowskiego 18/22",51.75259608,19.45366977));
-        placesList.add(new PlaceInfo("WydziaĹ‚ Technologii MateriaĹ‚owych i Wzornictwa TekstyliĂłw","Ĺ»eromskiego 116",51.75283733,19.4502742));
+        images = new ArrayList<>();
+        Resources resources = getResources();
+        images.add(resources.getDrawable(R.drawable.weeia));
+        images.add(resources.getDrawable(R.drawable.mech));
+        images.add(resources.getDrawable(R.drawable.chem));
+        images.add(resources.getDrawable(R.drawable.mech));
+        images.add(resources.getDrawable(R.drawable.mech_fabryka_inz));
+        images.add(resources.getDrawable(R.drawable.chem));
+        images.add(resources.getDrawable(R.drawable.ife));
+        images.add(resources.getDrawable(R.drawable.binoz));
+        images.add(resources.getDrawable(R.drawable.weeia));
+        images.add(resources.getDrawable(R.drawable.weeia));
+        images.add(resources.getDrawable(R.drawable.ipos));
+        images.add(resources.getDrawable(R.drawable.binoz));
+        images.add(resources.getDrawable(R.drawable.rekrutacja));
+        images.add(resources.getDrawable(R.drawable.wzornictwa));
+
+        names = new String[]{"Wydział Elektrotechniki Elektroniki Informatyki i Automatyki", "Wydział Mechaniczny", "Wydział Chemiczny", "Wydział Mechaniczny",
+                "Wydział Mechaniczny - Fabryka Inżynierów", "Instytut Chemi Ogólnej i Ekologicznej", "Centrum Kształcenia Międzynarodowego"
+                , "Wydział Biotechnologii i nauk o żywności", "Katedra aparatury przemysłowej", "Instytut Mechatroniki i Systemów Informatycznych",
+                "Wydział Inżynierii Procesowej i Ochrony środowiska", "Wydział Biotechnologii i nauk o żywności", "Rekrutacja", "Wydział Technologii Materiałowych i Wzornictwa Tekstyliów"};
+        addresses = new String[]{"Stefanowskiego 18/22", "Stefanowskiego 1/15", "Żeromskiego 116", "Stefanowskiego 1/15", "Stefanowskiego 2", "Żeromskiego 116", "Żwirki 36",
+                "Stefanowskiego", "Stefanowskiego 12/16", "Stefanowskiego 18/22", "Wólczańska 213", "Wólczańska 171/173", "Stefanowskiego 18/22", "Żeromskiego 116"};
+        lat = new double[]{51.75252299, 51.75284959, 51.75365549, 51.75373305, 51.75495143, 51.75424923, 51.75503501, 51.75472001, 51.75370351, 51.7537103, 51.75412276, 51.75448702, 51.75259608, 51.75283733};
+        lon = new double[]{19.45303313, 19.45258552, 19.45091179, 19.45180004, 19.45124336, 19.45077855, 19.45152691, 19.45264238, 19.4529498, 19.45394666, 19.45414356, 19.45450147, 19.45366977, 19.4502742};
+
+        for (int i = 0; i < names.length; i++) {
+            placesList.add(new PlaceInfo(names[i], addresses[i], lat[i], lon[i]));
+        }
 
     }
 
@@ -110,20 +143,20 @@ public class MainActivity extends AppCompatActivity {
         mMapController.setCenter(gPt);
     }
 
-    public void addMarkers() {
+    public void addMarker(int id) {
         mMapView.getOverlays().clear();
-        for(PlaceInfo info:placesList){
-            Marker marker = new Marker(mMapView);
-            marker.setPosition(info.getGeoPoint());
-            marker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_BOTTOM);
-            marker.setIcon(getResources().getDrawable(R.drawable.marker));
-            marker.setTitle(info.getTitle());
-            marker.setSubDescription(info.getSubDescription());
-            //marker.setImage(getResources().getDrawable(drawableID));
-            //mMapView.getOverlays().clear();
-            mMapView.getOverlays().add(marker);
-            mMapView.invalidate();
-        }
+        Marker marker = new Marker(mMapView);
+        GeoPoint point = placesList.get(id).getGeoPoint();
+        marker.setPosition(point);
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        marker.setIcon(getResources().getDrawable(R.drawable.marker_red));
+        marker.setTitle(placesList.get(id).getTitle());
+        marker.setSubDescription(placesList.get(id).getSubDescription());
+        marker.setImage(images.get(id));
+        mMapView.getOverlays().clear();
+        mMapView.getOverlays().add(marker);
+        mMapView.invalidate();
+        mMapController.animateTo(point);
     }
 
     // Request permissions to support Android Marshmallow and above devices  (api-23)
